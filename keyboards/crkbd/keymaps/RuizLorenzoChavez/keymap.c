59c4b10da81166b51a8bd758117a58654e4a902c
RuizLorenzoChavez <ruiz_chavez@dlsu.edu.ph>
2025-04-05 16:20:23 +0800

    fix: Replace LGUI to LGUI_T

--- keyboards/crkbd/keymaps/RuizLorenzoChavez/keymap.c
+++ keyboards/crkbd/keymaps/RuizLorenzoChavez/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_K,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
-                  QK_GESC, LGUI(KC_TAB), LT(_NUM, KC_SPC),    LT(_SYM, KC_ENT), LT(_NAV, KC_BSPC), LT(_FUN, KC_DEL)
+                  QK_GESC, LGUI_T(KC_TAB), LT(_NUM, KC_SPC),    LT(_SYM, KC_ENT), LT(_NAV, KC_BSPC), LT(_FUN, KC_DEL)
                              //`--------------------------'  `--------------------------'
     ),
 
